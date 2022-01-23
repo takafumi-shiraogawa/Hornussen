9805de89524c9d98cdaeb89dcef36b39e46eb525
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-01-23 14:22:20 +0900

    add a code of inverse design

---
+++ src/inverse_design/design.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+class Design_Tools():
+  """ Tools of inverse design """
+
+class Inverse_Design():
+  """ Inverse design based on chemical space of geometrically relaxed molecules """
