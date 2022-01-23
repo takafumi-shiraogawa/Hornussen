954814546d11d23caf08e4d59f4ccd2835dd4081
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-01-23 14:51:52 +0900

    add a generator of participation coefficients

--- src/inverse_design/design.py
+++ src/inverse_design/design.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
+import numpy as np
+import random
 
 class Design_Tools():
   """ Tools of inverse design """
+
+  def gener_init_part_coeff(random_seed, num_mol):
+    """ Generate inital participation coeffcients """
+
+    # Set initial participation coefficients
+    init_part_coeff = np.zeros(num_mol)
+
+    # Set random seed
+    random.seed(random_seed)
+
+    # Generate randomized initial participation coefficients
+    for i in range(num_mol):
+      init_part_coeff[i] = random.random()
+
+    return init_part_coeff
+
 
 class Inverse_Design():
   """ Inverse design based on chemical space of geometrically relaxed molecules """
