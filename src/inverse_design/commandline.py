159dd151c6b8f7cc4344fe380887a1789508c979
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-02-14 16:21:17 +0900

    add time measuring of design

--- src/inverse_design/commandline.py
+++ src/inverse_design/commandline.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
+from functools import wraps
+import time
 import inverse_design.design as ds
 import inverse_design.settings as iconf
 # from apdft.settings import Configuration
 
+
+def stop_watch(func):
+  """ Measure time """
+  @wraps(func)
+  def wrapper(*args, **kargs):
+    start = time.time()
+
+    result = func(*args, **kargs)
+
+    elapsed_time = time.time() - start
+
+    print("")
+    print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+    with open('elapsed_time.dat', 'w') as tfile:
+      tfile.write("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+    return result
+  return wrapper
+
+
+@stop_watch
 def ignition():
   geom_coordinate, mol_target_list, free_atom_energies = iconf.Option.get_inputs()
   design_target_property = iconf.Option.get_input_design()
