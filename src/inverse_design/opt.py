a27263b10ca0363df71ba854b375d40ff9c21e7e
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2023-03-15 10:36:03 +0900

    clean the code

--- src/inverse_design/opt.py
+++ src/inverse_design/opt.py
@@ -7,13 +7,12 @@ class line_searcher():
   # Perform a line search by a steepest descent minimization manner
   def steepest_descent(variable, gradient, grad_scale_factor = 1.0):
     """ Calculates updated variables.
-
 		Args:
-			variable:		A N array of variables
-      gradient:      A N array of derivatives of objective function with respect to given variables
-			grad_scale_factor:			A scale factor of gradients
+			variable          : A N array of variables
+      gradient          : A N array of derivatives of objective function with respect to given variables
+			grad_scale_factor : A scale factor of gradients
 		Returns:
-			next_variable: A N array of updated variables
+			next_variable : A N array of updated variables
 		"""
     return variable - grad_scale_factor * gradient
 
